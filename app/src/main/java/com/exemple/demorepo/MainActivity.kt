d23e854ad0e2d2b45de4893f914bdff586ee3977
umair shaik <software231@gmail.com>
2020-04-20 18:32:52 +0530

    SATA-1209 : Rename (#1)
    Description : Renamed my application class
    Testing Done : YES
    
    Co-authored-by: Umair Shaik <umshaik@deloitte.com>

--- app/src/main/java/com/exemple/demorepo/MainActivity.kt
+++ app/src/main/java/com/exemple/demorepo/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //just comment
+        //change 2
+        //change 3
     }
 }
